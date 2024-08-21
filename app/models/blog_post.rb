ef7f80d0c3e9060b9beaa087a0f2417acfd264af
audiosupp <ivangerasimyukmail@gmail.com>
2024-08-21 23:52:04 +0300

    test to render

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,8 @@
 class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  scope :draft, lambda { where(published_at: nil) }
+  scope :published, lambda { where("published_at <= ?", Time.current) }
+  scope :scheduled, lambda { where("published_at > ?", Time.current) }
 end
